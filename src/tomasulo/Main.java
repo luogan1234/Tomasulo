8c6041477f07e5c4af18dcba2e3bb48b28cd9d7d
tonygao <gaotong_sh@foxmail.com>
2017-06-06 16:25:35 +0800

    Init UI setting
    The basic UI setting

--- src/tomasulo/Main.java
+++ src/tomasulo/Main.java
@@ -5,49 +5,52 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
-public class Main /*extends Application*/ {
-/*    private TomasuloCore core;
+public class Main extends Application {
+    private TomasuloCore core;
 
     @Override
     public void start(Stage primaryStage) {
         try {
             core = new TomasuloCore();
 
-            Button btn = new Button();
-            btn.setText(core.bProperty().toString());
-            core.bProperty().addListener(new ChangeListener<Object>(){
-                @Override
-                public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
-                    btn.setText(core.bProperty().toString());
-                }
-            });
+//            Button btn = new Button();
+//            btn.setText(core.bProperty().toString());
+//            core.bProperty().addListener(new ChangeListener<Object>(){
+//                @Override
+//                public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
+//                    btn.setText(core.bProperty().toString());
+//                }
+//            });
+//
+//            btn.setOnAction(new EventHandler<ActionEvent>() {
+//                @Override
+//                public void handle(ActionEvent event) {
+//                    core.action();
+//                }
+//            });
 
-            btn.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    core.action();
-                }
-            });
-
-            StackPane root = new StackPane();
-            root.getChildren().add(btn);
-            Scene scene = new Scene(root, 400, 400);
+            AnchorPane root = FXMLLoader.load(getClass().getClassLoader().getResource("TomasuloUI.fxml"));
+//            root.getChildren().add(btn);
+            
+            Scene scene = new Scene(root, 640, 480);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }*/
+    }
 
     public static void main(String[] args) {
-    //    launch(args);
-    	test();
+        launch(args);
     }
     
     public static void test()
